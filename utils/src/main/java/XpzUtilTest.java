853801c2705c26891c4b4e876f5ab0d16b969c74
HappinessPursuer <83688869+HappinessPursuer@users.noreply.github.com>
2022-08-26 16:32:37 +0800

    第一次测试

---
+++ utils/src/main/java/XpzUtilTest.java
@@ -0,0 +1,13 @@
+/**
+ * @PROJECT_NAME: xjava
+ * @PACKAGE_NAME: PACKAGE_NAME
+ * @NAME: XpzUtilTest
+ * @DATETIME: 2022/8/26 16:31
+ * @author: XiaPengzhan
+ * @Description:
+ */
+public class XpzUtilTest {
+    public static String test(){
+        return "XpzUtilTest测试成功";
+    }
+}
